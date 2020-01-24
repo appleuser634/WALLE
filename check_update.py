8f2948665eb652743d1373cdaba35045ff189f76
appleuser634 <appleuser6348@gmail.com>
2020-01-24 06:58:10 +0000

    add change

--- check_update.py
+++ check_update.py
@@ -1,7 +1,16 @@
 import subprocess
+import time
 
-print("DID YOU UPDATE?")
-print("Hello!")
+i = 0
+while True:
+    
+    if i == 30: 
+        print("DID YOU UPDATE?")
 
-cmd = "git pull"
-subprocess.call(cmd, shell=True)
+        cmd = "git pull"
+        subprocess.call(cmd, shell=True)
+
+    print(i)
+    i += 1
+
+    time.sleep(1)
